dc10b4d681425edbe45869c7f31ad24185098fb8
Negin <negin.namiranian@gmail.com>
2017-09-09 20:25:08 -0700

    Script almost done. Just need to add the win condition.

--- Hangman-script.js
+++ Hangman-script.js
@@ -18,10 +18,13 @@ var current_word_string="";
 for (var i = 0; i <current_word.length; i++) {
 	current_word_string+=" "+current_word[i]+" ";
 	
+	
 }
+//document.querySelector("#current_word").innerHTML=current_word_string;
 var win_flag=0;
 var loss_flag=0;
 var wins=0;
+var losses=0;
 
 document.onkeyup=function(event) {
 
@@ -30,106 +33,94 @@ document.onkeyup=function(event) {
 	
 	var computer_choice_lower=computer_choice.toLowerCase();
 
-	console.log("Computer Choice: "+computer_choice);
-/*	if (loss_flag==1) {
-		random_number= Math.floor(Math.random() *9);
-		computer_choice=shows[random_number];
-		computer_choice_array=new Array(shows[random_number].length);
-		for (var i = 0; i < computer_choice_array.length; i++) {
-			computer_choice_array[i]=computer_choice.charAt(i).toLowerCase();
-		}
-		chances=12;
-		guess_string="";
-		current_word=new Array(shows[random_number].length);
+	console.log("Computer Choice second: "+computer_choice);
+	//Checking if a current pressed button had already been pressed before
+	var checkRepetition=0;
+	for (var i = 0; i < current_word.length; i++) {
+	 	if (userGuess===current_word[i] || guess_string.search(userGuess)!==-1) {
+	 		checkRepetition=1;
+	 	}
+	 } 
 
-		for (var i = 0; i < current_word.length; i++) {
-			current_word[i]="-";
-		}
-		var current_word_string="";
-		for (var i = 0; i <current_word.length; i++) {
-			current_word_string+=" "+current_word[i]+" ";
-			
-		}
-		loss_flag=0;
-	} */
 	
 
-	if (chances>0) {
-		if (computer_choice_lower.search(userGuess)!==-1) {
-			current_word_string="";
-			for (var i = 0; i <computer_choice_array.length ; i++) {
-				if (computer_choice_array[i]===userGuess) {
-					current_word[i]=userGuess;
+	if (1==1) {
+		if (chances>1) {
+			if (computer_choice_lower.search(userGuess)!==-1) {
+				current_word_string="";
+				for (var i = 0; i <computer_choice_array.length ; i++) {
+					if (computer_choice_array[i]===userGuess) {
+						current_word[i]=userGuess;
+					}
 				}
-			}
-			for (var i = 0; i <current_word.length; i++) 
-				current_word_string+=" "+current_word[i]+" ";
+				for (var i = 0; i <current_word.length; i++) 
+					current_word_string+=" "+current_word[i]+" ";
 
-			document.querySelector("#current_word").innerHTML=current_word_string;
-		}
-		else{
-
-			chances--;
-			guess_string+=" "+userGuess+" ";
-			document.querySelector("#guesses_so_far").innerHTML=guess_string;
-			document.querySelector("#guesses_left").innerHTML=chances;
-		}
-	}
-	else{
-		random_number= Math.floor(Math.random() *9);
-		computer_choice=shows[random_number];
-		computer_choice_array=new Array(shows[random_number].length);
-		for (var i = 0; i < computer_choice_array.length; i++) {
-			computer_choice_array[i]=computer_choice.charAt(i).toLowerCase();
-		}
-		chances=12;
-		guess_string="";
-		current_word=new Array(shows[random_number].length);
-
-		for (var i = 0; i < current_word.length; i++) {
-			current_word[i]="-";
-		}
-		var current_word_string="";
-		for (var i = 0; i <current_word.length; i++) {
-			current_word_string+=" "+current_word[i]+" ";
-			
-		}
-		document.querySelector("#current_word").innerHTML=current_word_string;
-	} 
-	
-	/*	if (computer_choice_lower.search(userGuess)!==-1 && chances>0) {
-			current_word_string="";
-			for (var i = 0; i <computer_choice_array.length ; i++) {
-				if (computer_choice_array[i]===userGuess) {
-					current_word[i]=userGuess;
-				}
-			}
-			for (var i = 0; i <current_word.length; i++) 
-				current_word_string+=" "+current_word[i]+" ";
-
-			document.querySelector("#current_word").innerHTML=current_word_string;
-			
-
-
-		}
-		else{
-			if (chances>0) {
-			chances--;
-			guess_string+=" "+userGuess+" ";
-			document.querySelector("#guesses_so_far").innerHTML=guess_string;
-			document.querySelector("#guesses_left").innerHTML=chances;
+				document.querySelector("#current_word").innerHTML=current_word_string;
 			}
 			else{
-				loss_flag=1;
+				current_word_string="";
+				
+				for (var i = 0; i <current_word.length; i++) 
+					current_word_string+=" "+current_word[i]+" ";
+
+				document.querySelector("#current_word").innerHTML=current_word_string;
+				
+				chances--;
+				guess_string+=" "+userGuess+" ";
+				document.querySelector("#guesses_so_far").innerHTML=guess_string;
+				document.querySelector("#guesses_left").innerHTML=chances;
+				console.log(" chances left: "+chances);
+				
+			}
+		}
+		
+		else if (chances==1) {
+			if (computer_choice_lower.search(userGuess)!==-1) {
+				current_word_string="";
+				for (var i = 0; i <computer_choice_array.length ; i++) {
+					if (computer_choice_array[i]===userGuess) {
+						current_word[i]=userGuess;
+					}
+				}
+				for (var i = 0; i <current_word.length; i++) 
+					current_word_string+=" "+current_word[i]+" ";
+
+				document.querySelector("#current_word").innerHTML=current_word_string;  
+			
+			}
+			else{
+				losses++;
+				chances=12;
+				random_number= Math.floor(Math.random() *9);
+				computer_choice=shows[random_number];
+				computer_choice_array=new Array(shows[random_number].length);
+				for (var i = 0; i < computer_choice_array.length; i++) 
+					computer_choice_array[i]=computer_choice.charAt(i).toLowerCase();
+				document.querySelector("#losses").innerHTML=losses;
+				guess_string="";
+				current_word_string="";
+				document.querySelector("#guesses_so_far").innerHTML=guess_string;
+				document.querySelector("#guesses_left").innerHTML="";
+				document.querySelector("#current_word").innerHTML=current_word_string;
+				console.log("first computer choice: "+computer_choice);
+				current_word=new Array(shows[random_number].length);
+				for (var i = 0; i < current_word.length; i++)
+						current_word[i]="-";
+				for (var i = 0; i <current_word.length; i++)
+					current_word_string+=" "+current_word[i]+" ";
+				document.querySelector("#current_word").innerHTML=current_word_string;
+
+
 
 			}
 
-
-		} */
-	
+		}
 
 
-
-
-
+	}
+	else{
+		alert("U have already guessed this letter");
+	}
+		
 }
